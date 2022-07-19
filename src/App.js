8e8f87d53a7715f00a69f22bcccbf5bdbb6a8f85
sogoyayo <sogo.oyerinde@gmail.com>
2022-07-19 19:57:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Title from './components/Title';
+// we will use setup for all the filename but always change the file path to the specific component
+import Setup from './examples/6-interceptors'
+// import './axios/global'
+
+
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup />
+    </main>
+  );
+}
+
+export default App;
